fe5d7832fad958801c99cd1c53b43200347e40d5
kshitijkumar-dev <kshitijkumar.dev@gmail.com>
2026-01-31 23:02:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById("btn");
+const memeImg = document.getElementById("memeImg");
+const memeTitle = document.getElementById("memeTitle");
+
+async function getMeme() {
+  try {
+    const response = await fetch("https://meme-api.com/gimme");
+    const data = await response.json();
+
+    memeImg.src = data.url;
+    memeTitle.innerText = data.title;
+  } catch (error) {
+    memeTitle.innerText = "Failed to load meme 😢";
+  }
+}
+
+btn.addEventListener("click", getMeme);
+
+// Load meme on page open
+getMeme();
